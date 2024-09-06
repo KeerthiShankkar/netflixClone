91c72b0d617bf292a5901cf4a3c5f9a026c40933
KeerthiShankkar <159803065+KeerthiShankkar@users.noreply.github.com>
2024-09-06 21:33:19 +0530

    Add files via upload

---
+++ homePage.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#first").click(function(){
+        $("#extensionOne").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#second").click(function(){
+        $("#extensionTwo").slideToggle();
+    });
+});
